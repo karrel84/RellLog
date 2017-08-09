b1419b70ff5d69dc498e8eb045a61df3cde29899
karrel <karrel84@naver.com>
2017-08-09 15:43:13 +0900

    Initial commit

---
+++ mylibrary/src/main/java/com/karrel/mylibrary/RLog.java
@@ -0,0 +1,108 @@
+package com.karrel.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by Rell on 2017. 8. 9..
+ */
+
+public class RLog {
+    private static boolean PRINT_ENABLED_LOG = true;
+    private static boolean SYSTEM_OUT_MODE = false;
+
+    static final String TAG = "Rell";
+
+    public static void setEnable(boolean enable) {
+        PRINT_ENABLED_LOG = enable;
+    }
+
+    public static void setSystemMode(boolean isSystemMode) {
+        SYSTEM_OUT_MODE = isSystemMode;
+    }
+
+    /**
+     * Log Level Error
+     **/
+    public static final void e(String message) {
+        if (!PRINT_ENABLED_LOG) return;
+        if (SYSTEM_OUT_MODE) {
+            System.out.println(buildSystemMsg(message));
+        } else {
+            Log.e(TAG, buildLogMsg(message));
+        }
+
+    }
+
+    /**
+     * Log Level Warning
+     **/
+    public static final void w(String message) {
+        if (!PRINT_ENABLED_LOG) return;
+        if (SYSTEM_OUT_MODE) {
+            System.out.println(buildSystemMsg(message));
+        } else {
+            Log.w(TAG, buildLogMsg(message));
+        }
+    }
+
+    /**
+     * Log Level Information
+     **/
+    public static final void i(String message) {
+        if (!PRINT_ENABLED_LOG) return;
+        if (SYSTEM_OUT_MODE) {
+            System.out.println(buildSystemMsg(message));
+        } else {
+            Log.i(TAG, buildLogMsg(message));
+        }
+    }
+
+    /**
+     * Log Level Debug
+     **/
+    public static final void d(String message) {
+        if (!PRINT_ENABLED_LOG) return;
+        if (SYSTEM_OUT_MODE) {
+            System.out.println(buildSystemMsg(message));
+        } else {
+            Log.d(TAG, buildLogMsg(message));
+        }
+    }
+
+    /**
+     * Log Level Verbose
+     **/
+    public static final void v(String message) {
+        if (!PRINT_ENABLED_LOG) return;
+        if (SYSTEM_OUT_MODE) {
+            System.out.println(buildSystemMsg(message));
+        } else {
+            Log.v(TAG, buildLogMsg(message));
+        }
+    }
+
+    private static String buildLogMsg(String message) {
+        StackTraceElement ste = Thread.currentThread().getStackTrace()[4];
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        sb.append(ste.getFileName().replace(".java", ""));
+        sb.append("::");
+        sb.append(ste.getMethodName());
+        sb.append("]");
+        sb.append(message);
+        return sb.toString();
+    }
+
+    private static String buildSystemMsg(String message) {
+        StackTraceElement ste = Thread.currentThread().getStackTrace()[3];
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        sb.append(ste.getFileName().replace(".java", ""));
+        sb.append("::");
+        sb.append(ste.getMethodName());
+        sb.append("]");
+        sb.append(message);
+        return sb.toString();
+    }
+
+}
